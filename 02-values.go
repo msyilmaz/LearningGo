da62195ba773c7f3fea73134e2573ad031c797fc
Safa Yilmaz <msafayilmaz@outlook.com>
2021-01-16 02:07:17 +0300

    first step into Go

---
+++ 02-values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("ms" + "yilmaz")
+
+	fmt.Println("10-2 =", 10-2)
+	fmt.Println("12.0/5.0 =", 12.0/5.0)
+
+	fmt.Println(!true)
+	fmt.Println(true || false)
+	fmt.Println(true && false)
+}
